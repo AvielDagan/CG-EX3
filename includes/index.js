ff6fe1585b77ebf36bd9693bea3279109b974c83
chen gutman <chengutman0@gmail.com>
2021-06-18 16:18:00 +0300

    logic

--- includes/index.js
+++ includes/index.js
@@ -2,8 +2,8 @@
 var pointArray = []
 var cubeArray = []
 var pyramidArray = []
-var cube
-var cubeArrayLength = 0
+var totalRes = []
+
 const organizeData = async () => {
     var slice;
     var file = document.getElementById('input-file').files;
@@ -53,7 +53,6 @@ const organizeData = async () => {
                 specificLine = specificLine.replace(')', '');
                 specificLine = specificLine.split('-')
                 cubeArray.push({ line: k, value: specificLine })
-                cubeArrayLength = cubeArrayLength + 1
             }
             console.log('cubeArray:', cubeArray)
             for (; t < lines.length - 1; t++) {
@@ -77,181 +76,168 @@ const organizeData = async () => {
         slice = file.mozSlice(0, file.size);
     }
     fileReader.readAsText(slice);
-    console.log('cubeArrayLength:', cubeArrayLength)
-    cube = await new Cube(0, 0, 400, 200);
-    await loop();
-
+    totalRes = [pointArray, cubeArray, pyramidArray]
+    return totalRes;
 }
 
-const Point3D = function (x, y, z) { this.x = x; this.y = y; this.z = z; };
-const Point2D = function (x, y) { this.x = x; this.y = y; };
+async function showCube() {
+    const Point3D = function (x, y, z) { this.x = x; this.y = y; this.z = z; };
+    const Point2D = function (x, y) { this.x = x; this.y = y; };
+    console.log('calling');
+    const result = await organizeData();
+    console.log("cube array",result[1].length);
+    const Cube = function (x, y, z, size) {
 
-const Cube = function (x, y, z, size) {
-    console.log('x, y, z, size', x, y, z, size)
+        console.log('x, y, z, size', x, y, z, size)
 
-    Point3D.call(this, x, y, z);
+        Point3D.call(this, x, y, z);
 
-    size *= 0.5;
+        size *= 0.5;
 
-    this.vertices = [new Point3D(x - size, y - size, z - size),
-    new Point3D(x + size, y - size, z - size),
-    new Point3D(x + size, y + size, z - size),
-    new Point3D(x - size, y + size, z - size),
-    new Point3D(x - size, y - size, z + size),
-    new Point3D(x + size, y - size, z + size),
-    new Point3D(x + size, y + size, z + size),
-    new Point3D(x - size, y + size, z + size)];
-    // for (let i = 0; i < cubeArray.length; i++) {
-    //     let faces = []
-    //     console.log('cubeArray[i]:', cubeArray[i])
-    // }
-    console.log('cubeArray:', cubeArray)
-    this.faces = [[0, 1, 2, 3], [0, 4, 5, 1], [1, 5, 6, 2], [3, 2, 6, 7], [0, 3, 7, 4], [4, 7, 6, 5]];
+        this.vertices = [new Point3D(x - size, y - size, z - size),
+        new Point3D(x + size, y - size, z - size),
+        new Point3D(x + size, y + size, z - size),
+        new Point3D(x - size, y + size, z - size),
+        new Point3D(x - size, y - size, z + size),
+        new Point3D(x + size, y - size, z + size),
+        new Point3D(x + size, y + size, z + size),
+        new Point3D(x - size, y + size, z + size)];
+        // for (let i = 0; i < cubeArray.length; i++) {
+        //     let faces = []
+        //     console.log('cubeArray[i]:', cubeArray[i])
+        // }
+        console.log('cubeArray:', cubeArray)
+        this.faces = [[0, 1, 2, 3], [0, 4, 5, 1], [1, 5, 6, 2], [3, 2, 6, 7], [0, 3, 7, 4], [4, 7, 6, 5]];
 
-};
+    };
 
-Cube.prototype = {
+    Cube.prototype = {
 
-    rotateX: function (radian) {
+        rotateX: function (radian) {
 
-        var cosine = Math.cos(radian);
-        var sine = Math.sin(radian);
+            var cosine = Math.cos(radian);
+            var sine = Math.sin(radian);
 
-        for (let index = this.vertices.length - 1; index > -1; --index) {
+            for (let index = this.vertices.length - 1; index > -1; --index) {
 
-            let p = this.vertices[index];
+                let p = this.vertices[index];
 
-            let y = (p.y - this.y) * cosine - (p.z - this.z) * sine;
-            let z = (p.y - this.y) * sine + (p.z - this.z) * cosine;
+                let y = (p.y - this.y) * cosine - (p.z - this.z) * sine;
+                let z = (p.y - this.y) * sine + (p.z - this.z) * cosine;
 
-            p.y = y + this.y;
-            p.z = z + this.z;
+                p.y = y + this.y;
+                p.z = z + this.z;
+
+            }
+        },
+
+        rotateY: function (radian) {
+
+            var cosine = Math.cos(radian);
+            var sine = Math.sin(radian);
+
+            for (let index = this.vertices.length - 1; index > -1; --index) {
+
+                let p = this.vertices[index];
+
+                let x = (p.z - this.z) * sine + (p.x - this.x) * cosine;
+                let z = (p.z - this.z) * cosine - (p.x - this.x) * sine;
+
+                p.x = x + this.x;
+                p.z = z + this.z;
+
+            }
 
         }
-    },
 
-    rotateY: function (radian) {
+    };
 
-        var cosine = Math.cos(radian);
-        var sine = Math.sin(radian);
+    var context = document.querySelector("canvas").getContext("2d");
+    var pointer = new Point2D(0, 0);
+    var cube = new Cube(0, 0, 400, 200);
+    var height = document.documentElement.clientHeight;
+    var width = document.documentElement.clientWidth;
 
-        for (let index = this.vertices.length - 1; index > -1; --index) {
+    function project(points3d, width, height) {
 
-            let p = this.vertices[index];
+        var points2d = new Array(points3d.length);
 
-            let x = (p.z - this.z) * sine + (p.x - this.x) * cosine;
-            let z = (p.z - this.z) * cosine - (p.x - this.x) * sine;
+        var focal_length = 200;
 
-            p.x = x + this.x;
-            p.z = z + this.z;
+        for (let index = points3d.length - 1; index > -1; --index) {
+
+            let p = points3d[index];
+
+            let x = p.x * (focal_length / p.z) + width * 0.5;
+            let y = p.y * (focal_length / p.z) + height * 0.5;
+
+            points2d[index] = new Point2D(x, y);
 
         }
 
-    }
-
-};
-
-var context = document.querySelector("canvas").getContext("2d");
-var pointer = new Point2D(0, 0);
-
-var height = document.documentElement.clientHeight;
-var width = document.documentElement.clientWidth;
-
-function project(points3d, width, height) {
-
-    var points2d = new Array(points3d.length);
-
-    var focal_length = 200;
-
-    for (let index = points3d.length - 1; index > -1; --index) {
-
-        let p = points3d[index];
-
-        let x = p.x * (focal_length / p.z) + width * 0.5;
-        let y = p.y * (focal_length / p.z) + height * 0.5;
-
-        points2d[index] = new Point2D(x, y);
+        return points2d;
 
     }
 
-    return points2d;
+    function loop() {
 
-}
+        window.requestAnimationFrame(loop);
 
-function project(points3d, width, height) {
+        height = document.documentElement.clientHeight;
+        width = document.documentElement.clientWidth;
 
-    var points2d = new Array(points3d.length);
+        context.canvas.height = height;
+        context.canvas.width = width;
 
-    var focal_length = 200;
+        context.fillStyle = "rgba(168,110,144,0.800000011920929)";
+        context.fillRect(0, 0, width, height);
+        context.strokeStyle = "#ffffff";
 
-    for (let index = points3d.length - 1; index > -1; --index) {
+        cube.rotateX(pointer.y * 0.0001);
+        cube.rotateY(-pointer.x * 0.0001);
 
-        let p = points3d[index];
+        context.fillStyle = "#0080f0";
 
-        let x = p.x * (focal_length / p.z) + width * 0.5;
-        let y = p.y * (focal_length / p.z) + height * 0.5;
+        var vertices = project(cube.vertices, width, height);
 
-        points2d[index] = new Point2D(x, y);
+        for (let index = cube.faces.length - 1; index > -1; --index) {
 
-    }
+            let face = cube.faces[index];
 
-    return points2d;
+            let p1 = cube.vertices[face[0]];
+            let p2 = cube.vertices[face[1]];
+            let p3 = cube.vertices[face[2]];
 
-}
+            let v1 = new Point3D(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
+            let v2 = new Point3D(p3.x - p1.x, p3.y - p1.y, p3.z - p1.z);
 
-function loop() {
+            let n = new Point3D(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
 
-    window.requestAnimationFrame(loop);
+            if (-p1.x * n.x + -p1.y * n.y + -p1.z * n.z <= 0) {
 
-    height = document.documentElement.clientHeight;
-    width = document.documentElement.clientWidth;
-
-    context.canvas.height = height;
-    context.canvas.width = width;
-
-    context.fillStyle = "rgba(168,110,144,0.800000011920929)";
-    context.fillRect(0, 0, width, height);
-    context.strokeStyle = "#ffffff";
-
-    cube.rotateX(pointer.y * 0.0001);
-    cube.rotateY(-pointer.x * 0.0001);
-
-    context.fillStyle = "#0080f0";
-
-    var vertices = project(cube.vertices, width, height);
-
-    for (let index = cube.faces.length - 1; index > -1; --index) {
-
-        let face = cube.faces[index];
-
-        let p1 = cube.vertices[face[0]];
-        let p2 = cube.vertices[face[1]];
-        let p3 = cube.vertices[face[2]];
-
-        let v1 = new Point3D(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
-        let v2 = new Point3D(p3.x - p1.x, p3.y - p1.y, p3.z - p1.z);
-
-        let n = new Point3D(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
-
-        if (-p1.x * n.x + -p1.y * n.y + -p1.z * n.z <= 0) {
-
-            context.beginPath();
-            context.moveTo(vertices[face[0]].x, vertices[face[0]].y);
-            context.lineTo(vertices[face[1]].x, vertices[face[1]].y);
-            context.lineTo(vertices[face[2]].x, vertices[face[2]].y);
-            context.lineTo(vertices[face[3]].x, vertices[face[3]].y);
-            context.closePath();
-            context.fill();
-            context.stroke();
+                context.beginPath();
+                context.moveTo(vertices[face[0]].x, vertices[face[0]].y);
+                context.lineTo(vertices[face[1]].x, vertices[face[1]].y);
+                context.lineTo(vertices[face[2]].x, vertices[face[2]].y);
+                context.lineTo(vertices[face[3]].x, vertices[face[3]].y);
+                context.closePath();
+                context.fill();
+                context.stroke();
+            }
         }
     }
+
+    loop();
+
+    window.addEventListener("click", (event) => {
+
+        pointer.x = event.pageX - width * 0.5;
+        pointer.y = event.pageY - height * 0.5;
+
+    });
 }
 
-// loop();
 
-window.addEventListener("click", (event) => {
-
-    pointer.x = event.pageX - width * 0.5;
-    pointer.y = event.pageY - height * 0.5;
-
-});
+function check() {
+    showCube();
+}
