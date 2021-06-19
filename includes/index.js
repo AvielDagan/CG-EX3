e950ec8d1701d2f8c8ee9a62d0cb66bc2862a0e0
AvielDagan <48187820+AvielDagan@users.noreply.github.com>
2021-06-19 15:47:21 +0300

    started zoom in

--- includes/index.js
+++ includes/index.js
@@ -42,27 +42,30 @@ const organizeData = async () => {
             for (; j < k - 1; j++) {
                 var specificLine = fileContent.split('\n')[j + 1]
                 specificLine = specificLine.replace('(', '');
+                specificLine = specificLine.replace('"', '');
                 specificLine = specificLine.replace(')', '');
                 specificLine = specificLine.split(',')
                 pointArray.push(specificLine)
             }
-            console.log('pointArray:', pointArray)
+            // console.log('pointArray:', pointArray)
             for (; k < t - 1; k++) {
                 var specificLine = fileContent.split('\n')[k + 1]
+                specificLine = specificLine.replace('"', '');
                 specificLine = specificLine.replace('(', '');
                 specificLine = specificLine.replace(')', '');
                 specificLine = specificLine.split('-')
                 cubeArray.push(specificLine)
             }
-            console.log('cubeArray:', cubeArray)
+            // console.log('cubeArray:', cubeArray)
             for (; t < lines.length - 1; t++) {
                 var specificLine = fileContent.split('\n')[t + 1]
                 specificLine = specificLine.replace('(', '');
+                specificLine = specificLine.replace('"', '');
                 specificLine = specificLine.replace(')', '');
                 specificLine = specificLine.split('-')
                 pyramidArray.push(specificLine)
             }
-            console.log('pyramidArray:', pyramidArray)
+            // console.log('pyramidArray:', pyramidArray)
         }
     }
 
@@ -79,33 +82,20 @@ const organizeData = async () => {
     totalRes = [pointArray, cubeArray, pyramidArray]
     return totalRes;
 }
-function showCube(shape) {
-    console.log("cube array size", shape.length)
+function showCube(shape, points, zoomInFlag) {
     const Point3D = function (x, y, z) { this.x = x; this.y = y; this.z = z; };
     const Point2D = function (x, y) { this.x = x; this.y = y; };
 
     const Cube = function (x, y, z, size) {
 
-        console.log('x, y, z, size', x, y, z, size)
 
         Point3D.call(this, x, y, z);
 
         size *= 0.5;
-
-        this.vertices = [new Point3D(x - size, y - size, z - size),
-        new Point3D(x + size, y - size, z - size),
-        new Point3D(x + size, y + size, z - size),
-        new Point3D(x - size, y + size, z - size),
-        new Point3D(x - size, y - size, z + size),
-        new Point3D(x + size, y - size, z + size),
-        new Point3D(x + size, y + size, z + size),
-        new Point3D(x - size, y + size, z + size)];
-        // for (let i = 0; i < cubeArray.length; i++) {
-        //     let faces = []
-        //     console.log('cubeArray[i]:', cubeArray[i])
-        // }
-        this.faces = [[0, 1, 2, 3], [0, 4, 5, 1], [1, 5, 6, 2], [3, 2, 6, 7], [0, 3, 7, 4], [4, 7, 6, 5]];
-
+        console.log('points:', points)
+        this.vertices = points
+        console.log('this.vertices:', this.vertices)
+        this.faces = shape
     };
 
     Cube.prototype = {
@@ -190,8 +180,8 @@ function showCube(shape) {
         context.fillRect(0, 0, width, height);
         context.strokeStyle = "#ffffff";
 
-        cube.rotateX(pointer.y * 0.0001);
-        cube.rotateY(-pointer.x * 0.0001);
+        // cube.rotateX(pointer.y * 0.0001);
+        // cube.rotateY(-pointer.x * 0.0001);
 
         context.fillStyle = "#0080f0";
 
@@ -204,7 +194,6 @@ function showCube(shape) {
             let p1 = cube.vertices[face[0]];
             let p2 = cube.vertices[face[1]];
             let p3 = cube.vertices[face[2]];
-
             let v1 = new Point3D(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
             let v2 = new Point3D(p3.x - p1.x, p3.y - p1.y, p3.z - p1.z);
 
@@ -226,27 +215,84 @@ function showCube(shape) {
 
     loop();
 
-    window.addEventListener("click", (event) => {
+    // window.addEventListener("click", (event) => {
 
-        pointer.x = event.pageX - width * 0.5;
-        pointer.y = event.pageY - height * 0.5;
+    //     pointer.x = event.pageX - width * 0.5;
+    //     pointer.y = event.pageY - height * 0.5;
 
-    });
+    // });
 }
 
+function organizePoints(points, size = null) {
+    const Point3D = function (x, y, z) { this.x = x; this.y = y; this.z = z; };
+    var objOfPoints = []
+    console.log('size:', size)
+    if (!size) {
+        for (let i = 0; i < points.length; ++i) {
+            let x = points[i][0]
+            x = x.replace('"', '');
+            x = parseInt(x)
+            let y = points[i][1]
+            y = y.replace('"', '');
+            y = parseInt(y)
+            let z = points[i][2]
+            z = z.replace('"', '');
+            z = parseInt(z)
+            let point = new Point3D(x, y, z)
+            objOfPoints.push(point)
+        }
+    } else {
+        for (let i = 0; i < points.length; ++i) {
+            // let x = parseInt(points[i][0])
+            // let y = parseInt(points[i][1])
+            // y = parseInt(y * size)
+            let x = points[i][0] * size
+            x = parseInt(x)
+            let y = points[i][1] * size
+            y = parseInt(y)
+            let z = points[i][2] * size
+            // z = parseInt(z * size)
+            let point = new Point3D(x, y, z)
+            objOfPoints[i] = point
+            // objOfPoints[i] = points[i]
+        }
+        objOfPoints = objOfPoints.splice(0, 8)
+        console.log('objOfPoints in zoo,=m:', objOfPoints)
+    }
+    console.log('before return objOfPoints:', objOfPoints)
+    return objOfPoints
+}
 
-async function check() {
+async function zoomIn() {
     try {
         var result = await organizeData()
         setTimeout(function () {
             var shape = result[1]
-            console.log("shape", result[1][1][1])
-            showCube(shape);
+            var points = result[0]
+            var zoomInFlag = true
+            var zoomPoints = organizePoints(points, 1.1)
+            showCube(shape, zoomPoints, zoomInFlag);
         }, 1000);
 
     }
     catch (e) {
         console.log('e:', e)
     }
+}
 
+async function start() {
+    try {
+        var result = await organizeData()
+        setTimeout(function () {
+            var shape = result[1]
+            var points = result[0]
+            points = organizePoints(points)
+            var zoomInFlag = false
+            showCube(shape, points, zoomInFlag);
+        }, 1000);
+
+    }
+    catch (e) {
+        console.log('e:', e)
+    }
 }
